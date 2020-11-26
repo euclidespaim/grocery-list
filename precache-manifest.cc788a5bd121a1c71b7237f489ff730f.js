959ccd2e03b8edccb6f4f7677624d7900f54fdb0
euclidespaim <euclidespaim@gmail.com>
2020-11-26 09:17:32 -0300

    Updates

---
+++ precache-manifest.cc788a5bd121a1c71b7237f489ff730f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18c462abffee84ed9f14d26639dd496",
+    "url": "/grocery-list/index.html"
+  },
+  {
+    "revision": "d417544f1a8973ee682c",
+    "url": "/grocery-list/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/grocery-list/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-list/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d417544f1a8973ee682c",
+    "url": "/grocery-list/static/js/main.de9b90e3.chunk.js"
+  },
+  {
+    "revision": "442f1a7391b3cc56a3ca",
+    "url": "/grocery-list/static/js/runtime-main.a51afe8e.js"
+  }
+]);
